77eaed822aa2224d6cca8917211fde67f130d92c
Akshay kumar <akshay@capitall.io>
2023-06-22 17:22:57 +0530

    change home

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 										<div class="header-nav-main header-nav-main-square header-nav-main-font-lg-upper header-nav-main-effect-2 header-nav-main-sub-effect-1">
 											<nav class="collapse header-mobile-border-top">
 												<ul class="nav nav-pills" id="mainNav">
-													<li class="dropdown"><a class="dropdown-item dropdown-toggle active" href="index.php">Home</a></li>
+													<li class="dropdown"><a class="dropdown-item dropdown-toggle active" href="index.php">home</a></li>
 													<li class="dropdown"><a class="dropdown-item dropdown-toggle active" href="data-room.php">dataroom</a></li>
 													<li class="dropdown">
 														<a class="dropdown-item dropdown-toggle" href="team.php">Our Team</a>
